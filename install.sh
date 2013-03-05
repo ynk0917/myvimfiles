d8ea62019112499849910a90c4ddca1952f64d20
NienKun Yeh <akun@akun-desktop.(none)>
2013-03-05 12:06:34 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+current_directory=`pwd`
 if [ -f ~/.vimrc ];then
     mv ~/.vimrc ~/.vimrc.bak
+    rm ~/.vimrc
 fi
-ln -s 'pwd' ~/.vim
-ln -s .vimrc ~/.vimrc
+if [ -d ~/.vim ];then
+    mv ~/.vim ~/.vim_bak
+    rm ~/.vim
+fi
+ln -s $current_directory ~/.vim
+ln -s $current_directory/.vimrc ~/.vimrc
 ./update.sh
